a7904c9a01c96c680a3e77d7197b1adeadff2cfd
slavaider <slavaider1@gmail.com>
2021-09-21 21:32:51 +0500

    fix: fix ts

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import express, {Express} from 'express';
 import http from 'http';
 import next, {NextApiHandler} from 'next';
@@ -5,7 +6,6 @@ import socketio from 'socket.io';
 import cors from 'cors';
 import IUser from "../src/interfaces/user";
 import IMessage from "../src/interfaces/message";
-// import IMessage from "../src/interfaces/message";
 
 const port: number = parseInt(process.env.PORT || '3000', 10);
 const dev: boolean = process.env.NODE_ENV !== 'production';
@@ -31,14 +31,12 @@ nextApp.prepare().then(async () => {
             };
             socket.join(user.room);
             if (Object.prototype.hasOwnProperty.call(users, user.room)) {
-                // @ts-ignore
                 users[user.room].push(newUser);
             } else {
                 Object.assign(users, {[user.room]: [newUser]});
                 Object.assign(messages, {[user.room]: []});
             }
             socket.broadcast.to(user.room).emit('add user', newUser);
-            // @ts-ignore
             return cb(users[user.room], messages[user.room], newUser);
         });
 
@@ -47,10 +45,8 @@ nextApp.prepare().then(async () => {
         });
 
         socket.on('send message', (message: IMessage, cb) => {
-            // @ts-ignore
             messages[message.room].push(message);
             socket.broadcast.to(message.room).emit('add message', message);
-            // @ts-ignore
             cb(messages[message.room])
         });
 
