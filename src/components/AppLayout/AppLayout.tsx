33c8c74df2cc31e0b40a26a3e59e3f20551fbe17
slavaider <slavaider1@gmail.com>
2021-10-05 20:49:33 +0500

    fix: fix path

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -8,8 +8,8 @@ import useSocket from '../../hooks/useSocket';
 
 const AppLayout: FC = ({ children }) => {
   let host = 'http://localhost:3000';
-  if (process.browser) {
-    host = window.location.origin;
+  if (window) {
+    host = window.location.origin.replace(/^http/, 'ws');
   }
   const socket = useSocket(host);
 
